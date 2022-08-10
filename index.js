f8e549a713d2d107fbe80ec1fdb7c9852cbd08de
Benny Sufrin <bennysuf@gmail.com>
2022-08-09 21:18:46 -0400

    commit tree 797b6c75be64b1b045168f27d8a861834e289ae3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(set = "roller-skate") {
+    return `This Saturday, I want to ${set}!`;
+}
+function mondayWork(cam = 'go to the office') {
+    return `This Monday, I will ${cam}.`;
+}
+function wrapAdjective(p = '*') {
+   return function name(s = 'special') {
+        return `You are ${p}${s}${p}!`;
+    }
+}
